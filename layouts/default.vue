77e966d118c6b393fac7c40ee03cf3a7a5be3383
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2019-02-24 13:26:03 +0800

    practicing

--- layouts/default.vue
+++ layouts/default.vue
@@ -16,19 +16,11 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">Home
-                <span class="sr-only">(current)</span>
-              </a>
+            <li class="nav-item">
+               <nuxt-link class="nav-link" to="/">home</nuxt-link>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              <nuxt-link class="nav-link" to="/form">Form</nuxt-link>
             </li>
           </ul>
         </div>
